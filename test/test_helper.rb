6d0480fd26d077bf621a381f2073df52a321ec2a
Kerri Miller <kerrizor@kerrizor.com>
2013-09-29 12:08:48 -0700

    add assert_change and refute_change methods to MiniTest.. its likely these could be moved into a gem of their own at some point

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,20 @@ require 'minitest/unit'
 require 'minitest/autorun'
 require 'minitest/pride'
 
+class MiniTest::Unit::TestCase
+  def assert_change(block)
+    before = block.call
+
+    yield
+
+    refute_equal before, block.call
+  end
+
+  def refute_change(block)
+    before = block.call
+
+    yield
+
+    assert_equal before, block.call
+  end
+end

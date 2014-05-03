8dddcc3a3a082d6b73826c026c9afb2634b3698e
Kerri Miller <kerrizor@kerrizor.com>
2014-05-03 06:47:24 -0700

    extends #deck! to return more than one card at a time

--- lib/jeu_de_cartes/deck.rb
+++ lib/jeu_de_cartes/deck.rb
@@ -18,8 +18,14 @@ module JeuDeCartes
       self
     end
 
-    def deal!
-      cards.shift
+    def deal!(number_to_deal = 1)
+      hand = []
+
+      number_to_deal.times do
+        hand << cards.shift
+      end
+
+      hand
     end
 
     def card_count
@@ -27,6 +33,7 @@ module JeuDeCartes
     end
 
     private
+
     def random_location_in_deck(card_count)
       dist = Rubystats::NormalDistribution.new(card_count, 5)
       dist.rng.to_i
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -17,10 +17,21 @@ class DeckTest < MiniTest::Unit::TestCase
     refute_same @deck.cards.first, @deck.cut!.cards.first
   end
 
-  def test_deal_returns_one_card
-    card = @deck.deal!
+  def test_deal_without_options_returns_one_card
+    hand = @deck.deal!
 
-    assert_instance_of JeuDeCartes::Card, card
+    assert_instance_of Array, hand
+    assert_instance_of JeuDeCartes::Card, hand.first
+  end
+
+  def test_deal_accepts_number_of_cards_to_return_param
+    hand = @deck.deal!(5)
+
+    assert_instance_of Array, hand
+
+    hand.each do |card|
+      assert_instance_of JeuDeCartes::Card, card
+    end
   end
 
   def test_deal_removes_one_card
